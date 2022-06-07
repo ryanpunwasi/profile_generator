12bdc262d4179452feda0ac4949fe45c617eac87
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-07 15:37:50 -0400

    Used nested asynchronous calls to build series of survey questions and output answers

--- survey.js
+++ survey.js
@@ -5,3 +5,20 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+rl.question('What\'s your name? ', (name) => {
+  rl.question('What\'s your favorite sport? ', (sport) => {
+    rl.question('What\'s your favorite food? ', (food) => {
+      rl.question('What country do you live in? ', (country) => {
+        rl.question('What\'s your favorite tv show? ', (tv) => {
+          rl.question('What\'s your favorite music genre? ', (music) => {
+            rl.question('What\'s your current profession? ', (profession) => {
+              console.log(`${name} is a(n) ${profession} that lives in ${country} who enjoys playing ${sport} and watching ${tv} while eating ${food} and listening to ${music} music.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
